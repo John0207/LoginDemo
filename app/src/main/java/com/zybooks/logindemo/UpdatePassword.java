c661ae050b5d7ad56cb9b7b31ee7595b6383c48b
John Deluccia <john.deluccia@snhu.edu>
2020-07-22 23:52:00 -0400

    fully working file storage with firebase, tutorial 10

--- app/src/main/java/com/zybooks/logindemo/UpdatePassword.java
+++ app/src/main/java/com/zybooks/logindemo/UpdatePassword.java
@@ -1,15 +1,14 @@
 package com.zybooks.logindemo;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -23,9 +22,6 @@ public class UpdatePassword extends AppCompatActivity {
     private FirebaseUser firebaseUser;
     private FirebaseAuth firebaseAuth;
 
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,34 +32,32 @@ public class UpdatePassword extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        //get instance of firebase user from firebase auth
         firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
-
 
         update.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
 
                 String userPasswordNew = newPassword.getText().toString();
                 firebaseUser.updatePassword(userPasswordNew).addOnCompleteListener(new OnCompleteListener<Void>() {
                     @Override
                     public void onComplete(@NonNull Task<Void> task) {
                         if(task.isSuccessful()){
-                            Toast.makeText(UpdatePassword.this, "Password Changed!", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(UpdatePassword.this, "Password Changed", Toast.LENGTH_SHORT).show();
                             finish();
                         }else{
-                            Toast.makeText(UpdatePassword.this, "Password Change Failed", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(UpdatePassword.this, "Password Update Failed", Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
             }
         });
-        //TODO: add change button for name, email, age
 
 
     }
+
     @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item) {
 
         switch (item.getItemId()){
             case android.R.id.home:
